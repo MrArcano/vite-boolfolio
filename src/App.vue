43b9e223b89354c3c7457d258f47274700ebb480
MrArcano <salvatore-dario@hotmail.it>
2023-12-06 16:43:13 +0100

    [add]: card with data project

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,47 @@
 <script>
+import axios from 'axios';
+import { store } from './data/store';
+import ProjectCard from './components/ProjectCard.vue';
+
 export default {
   name:'App',
+  components:{
+    ProjectCard
+  },
   data() {
     return {
-      
+      projects:[],
     }
   },
   methods: {
-    
+    getAPI(){
+      axios.get(store.ApiUrl)
+        .then((response) => {
+          // handle success
+          console.log(response.data.data);
+          this.projects = response.data.data;
+        })
+        .catch((error) => {
+          // handle error
+          console.log(error);
+        })
+    }
   },
   mounted() {
-    
+    this.getAPI();
   },
 }
 </script>
 
 <template>
-  <h1>Vue</h1>
+  <div class="container my-5">
+    <h1 class="text-center">Vite Boolfolio</h1>
+    <div class="row">
+      <div class="col-6" v-for="project in projects" v-bind:key="project.id">
+        <ProjectCard :project="project"/> 
+      </div>
+    </div>
+  </div>
 </template>
 
 
