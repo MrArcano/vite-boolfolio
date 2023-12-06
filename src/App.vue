d99b144ffb6cea06eec37428ffb503076abf6edc
MrArcano <salvatore-dario@hotmail.it>
2023-12-06 15:52:24 +0100

    [init]

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  name:'App',
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    
+  },
+  mounted() {
+    
+  },
+}
+</script>
+
+<template>
+  <h1>Vue</h1>
+</template>
+
+
+<style>
+
+</style>
